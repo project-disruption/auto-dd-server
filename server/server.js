af19d4efadda79e74af51fa4b7fcf3a7c903d9fe
T Pearse <t.pearse@gmail.com>
2017-10-01 18:53:08 +1300

    Added a new 'route' to extract P&L by individual line over the historical period. Having issues with parsing. Will recommit when completed - or any pointers let me know.

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get('/invoices', (req, res) => {
   })
     .then((invoices) => {
       invoices = invoices.sort((invoice1, invoice2) => {
-        const date1 = moment(invoice1.Date) 
+        const date1 = moment(invoice1.Date)
         const date2 = moment(invoice2.Date)
         if(date1.isBefore(date2)) {
           return -1
@@ -35,18 +35,23 @@ app.get('/invoices', (req, res) => {
     })
 })
 
-app.get('/profitandloss', (req, res) => {
+
+// In this block I'm trying to split out a P&L report by reporting line
+app.get('/profitandlossbyline', (req, res) => {
   const fromDate = req.query.fromDate
   const toDate = req.query.toDate
+  const line = req.query.line
   xeroClient.core.reports.generateReport({
     id: 'ProfitAndLoss',
     params: {
       fromDate, toDate
     }
   })
-    .then((report) => {
-      res.send(report)
-    })
+  .then(report) => {
+    report === report.ReportID;
+    console.log("User Name:",report.ReportID);
+    res.send(report)
+  }
 })
 
 function getReport(fromDate, toDate) {
@@ -62,24 +67,45 @@ function getField (report, field) {
   switch (field) {
     case 'sales':
       return Number(report.Rows.find((row) => row.Title === 'Income').Rows.find((row) => row.RowType === 'SummaryRow').Cells[1].Value)
+    case 'Total Income':
+      return report.Rows
     default:
       return null
   }
 }
 
+
+
+app.get('/profitandloss', (req, res) => {
+  const fromDate = req.query.fromDate
+  const toDate = req.query.toDate
+  xeroClient.core.reports.generateReport({
+    id: 'ProfitAndLoss',
+    params: {
+      fromDate, toDate
+    }
+  })
+    .then((report) => {
+      res.send(report)
+    })
+})
+
+
+
 app.get('/joinprofitandloss', (req, res) => {
-  const fromDate1 = '2015-01-01'
-  const toDate1 = '2015-12-31'
-  const fromDate2 = '2016-01-01'
-  const toDate2 = '2016-12-31'
+  const fromDate1 = '2016-01-01'
+  const toDate1 = '2016-12-31'
+  const fromDate2 = '2017-01-01'
+  const toDate2 = '2017-12-31'
 
   Promise.all([
     getReport(fromDate1, toDate1),
     getReport(fromDate2, toDate2)
   ])
     .then((reports) => {
-      const totalSales = getField(reports[0], 'sales') + getField(reports[1], 'sales')
-      res.json({ totalSales })
+      res.send(reports)
+    // const totalSales = getField(reports[0], 'sales') + getField(reports[1], 'sales')
+    //  res.json({ totalSales })
     })
 
 })
